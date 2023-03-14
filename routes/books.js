327db82bc42ef9d5e4f0b62ae9205672134042f6
Egoitz G <egoitz.g@fptxurdinaga.com>
2023-03-14 11:08:38 +0100

    Implementacion de los endpoints de libros conectando con base de datos

--- routes/books.js
+++ routes/books.js
@@ -5,58 +5,71 @@ const books = require("../util/data");
 const bookService = require("../services/bookService");
 
 router.get("/", async function (req, res, next) {
-	// res.status(200).json(books);
 	try {
-		res.status(200).json(await bookService.getMultiple(req.query.page));
-	} catch (err){
+		res.status(200).json(await bookService.getMultiple());
+	} catch (err) {
 		console.error("Error while getting list of books ", err.message);
-		next(err);
+		res.sendStatus(501);
 	}
 });
 
-router.get("/:id", function (req, res) {
-	let book = books.find(function (item) {
-		return item.id == req.params.id;
-	});
+router.get("/:id", async function (req, res, next) {
+	/*	let book = books.find(function (item) {
+			return item.id == req.params.id;
+		});
 
-	book ? res.status(200).json(book) : res.sendStatus(404);
+		book ? res.status(200).json(book) : res.sendStatus(404);
+	*/
+	let id = req.params.id;
+
+	let bookArray = await bookService.getOneBook(id);
+
+	if (bookArray.data.length > 0) {
+		res.status(200).json(bookArray.data[0]);
+	} else {
+		res.status(404).json("{msg: No hay un libro con ese id}");
+	}
 });
 
-router.post("/", function (req, res) {
-	const { title, author, finished } = req.body;
+router.post("/", async function (req, res) {
+	const {
+		title,
+		author,
+		finished
+	} = req.body;
 
 	let book = {
-		id: books.length + 1,
 		title: title,
 		author: author,
-		finished: finished !== undefined ? finished : false,
-		createdAt: new Date(),
+		finished: finished !== undefined ? finished : false
 	};
 
-	books.push(book);
+	// books.push(book);
+	const created = await bookService.createBook(book);
+	const idCreated = created.data.insertId;
+	const createdBook = await bookService.getOneBook(idCreated);
 
-	res.status(201).json(book);
+
+	res.status(201).json(createdBook);
 });
 
-router.put("/:id", function (req, res) {
-	let book = books.find(function (item) {
-		return item.id == req.params.id;
-	});
+router.put("/:id", async function (req, res) {
+	const paramId = req.params.id;
+	const getBook = await bookService.getOneBook(paramId);
+	if (getBook.data.length > 0) {
+		const { title, author, finished} = req.body;
+		let updatedBook = getBook.data[0];
 
-	if (book) {
-		const { title, author, finished } = req.body;
+		updatedBook.title = title !== undefined ? title : updatedBook.title;
+		updatedBook.author = author !== undefined ? author : updatedBook.author;
+		updatedBook.finished = finished !== undefined ? finished : updatedBook.finished;
 
-		let updated = {
-			id: book.id,
-			title: title !== undefined ? title : book.title,
-			author: author !== undefined ? author : book.author,
-			finished: finished !== undefined ? finished : book.finished,
-			createdAt: book.createdAt,
-		};
+		const data = await bookService.modifyBook(updatedBook);
 
-		books.splice(books.indexOf(book), 1, updated);
+		console.debug("retorno:");
+		console.debug(data);
+		res.status(201).json(data);
 
-		res.sendStatus(204);
 	} else {
 		res.sendStatus(404);
 	}
--- services/bookService.js
+++ services/bookService.js
@@ -2,16 +2,18 @@ const db = require('./db');
 const helper = require('../helper');
 const config = require('../config');
 
-async function getMultiple(page = 1){
-  const offset = helper.getOffset(page, config.listPerPage);
+async function getMultiple(page = 1) {
+  //const offset = helper.getOffset(page, config.listPerPage);
   // const rows = await db.query(
   //   `SELECT * 
   //   FROM books LIMIT ${offset},${config.listPerPage}`
   // );
   const rows = await db.query(`SELECT * FROM books`);
-  
+
   const data = helper.emptyOrRows(rows);
-  const meta = {page};
+  const meta = {
+    page
+  };
 
   return {
     data,
@@ -19,6 +21,46 @@ async function getMultiple(page = 1){
   }
 }
 
+async function getOneBook(id) {
+
+  const rows = await db.query('SELECT * FROM books WHERE id=' + id + ';');
+
+  const data = helper.emptyOrRows(rows);
+
+  return {
+    data
+  }
+}
+
+async function createBook(book) {
+
+  let sql = "INSERT INTO `books` (`title`, `author`, `finished`, `createdAt`) VALUES (";
+  sql += "'"+book.title+"','"+book.author+"','"+book.finished+"'";
+  sql += ", current_timestamp());";
+
+  console.debug(sql);
+
+  const rows = await db.query(sql);
+
+  const data = helper.emptyOrRows(rows);
+  return {data}
+
+}
+
+async function modifyBook(book){
+
+  let sql = "UPDATE `books` SET `title` = ";
+  sql+= "'" + book.title +"', `author` = '" + book.author + "', `finished` = ' "+ book.finished +"' WHERE `books`.`id` =" + book.id + ";";
+
+  const rows = await db.query(sql);
+  const data = helper.emptyOrRows(rows);
+
+  return { data}
+}
+
 module.exports = {
-  getMultiple
+  getMultiple,
+  getOneBook,
+  createBook,
+  modifyBook
 }
